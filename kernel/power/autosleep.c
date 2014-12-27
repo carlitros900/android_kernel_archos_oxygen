2fc07695b68df372ada27a414ba41240cc307618
yank555-lu <yank555.lu@gmail.com>
2014-12-27 04:03:42 +0100

    kernel/power/powersuspend: cumulative update to version 1.6 (faux123/Yank555.lu)
    - autosleep hook removed, not working on shamu
    - autosleep and hybrid modes removed
    - panel mode is now default
    - debug output switchable in defconfig
    
    Signed-off-by: Swapnil Solanki <swapnil133609@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -11,10 +11,6 @@
 #include <linux/pm_wakeup.h>
 #include <mtk_hibernate_core.h>
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
 #include "power.h"
 
 #define HIB_AUTOSLEEP_DEBUG 1
@@ -138,16 +134,8 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
-#ifdef CONFIG_POWERSUSPEND
-		// Yank555.lu : add hook to handle powersuspend tasks (sleep)
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
-#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
-#ifdef CONFIG_POWERSUSPEND
-		// Yank555.lu : add hook to handle powersuspend tasks (wakeup)
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE);
-#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
