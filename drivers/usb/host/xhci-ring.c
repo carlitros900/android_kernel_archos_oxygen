3ad452cf23d0e7dfb83387155c8363c19ed4f05a
Mathias Nyman <mathias.nyman@linux.intel.com>
2015-09-21 17:46:16 +0300

    xhci: change xhci 1.0 only restrictions to support xhci 1.1
    [ Upstream commit dca7794539eff04b786fb6907186989e5eaaa9c2 ]
    
    Some changes between xhci 0.96 and xhci 1.0 specifications forced us to
    check the hci version in code, some of these checks were implemented as
    hci_version == 1.0, which will not work with new xhci 1.1 controllers.
    
    xhci 1.1 behaves similar to xhci 1.0 in these cases, so change these
    checks to hci_version >= 1.0
    
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -3487,7 +3487,7 @@ int xhci_queue_ctrl_tx(struct xhci_hcd *xhci, gfp_t mem_flags,
 #ifdef CONFIG_USB_XHCI_MTK
 	if (1) {
 #else
-	if ((xhci->hci_version == 0x100) || (xhci->quirks & XHCI_MTK_HOST)) {
+	if ((xhci->hci_version >= 0x100) || (xhci->quirks & XHCI_MTK_HOST)) {
 #endif
 		if (urb->transfer_buffer_length > 0) {
 			if (setup->bRequestType & USB_DIR_IN)
