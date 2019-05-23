302e3508186887b939087d38269e962899d158bd
Sahitya Tummala <stummala@codeaurora.org>
2019-05-23 09:49:17 +0530

    f2fs: add error prints for debugging mount failure
    Add error prints to get more details on the mount failure.
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2704,14 +2704,14 @@ try_onemore:
 	/* setup f2fs internal modules */
 	err = build_segment_manager(sbi);
 	if (err) {
-		f2fs_err(sbi, "Failed to initialize F2FS segment manager (%d)",
-			 err);
+		f2fs_msg(sb, KERN_ERR,
+			"Failed to initialize F2FS segment manager (%d)", err);
 		goto free_sm;
 	}
 	err = build_node_manager(sbi);
 	if (err) {
-		f2fs_err(sbi, "Failed to initialize F2FS node manager (%d)",
-			 err);
+		f2fs_msg(sb, KERN_ERR,
+			"Failed to initialize F2FS node manager (%d)", err);
 		goto free_nm;
 	}
 
